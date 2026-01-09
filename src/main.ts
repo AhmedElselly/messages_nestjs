c2e2dcc7a4ce2425bf4594dfe48a2f8b77716f75
ahmed elselly <ahmedelselly87@gmail.com>
2026-01-09 16:35:16 +0200

    added DI messagesServices as Injectable for messagesController class as well as messagesRepository class

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { MessagesModule } from './messages/messages.module';
-import { ValidationPipe } from '@nestjs/common';
-
-async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
-  const globalPipes = new ValidationPipe();
-  app.useGlobalPipes(globalPipes);
-  await app.listen(process.env.PORT ?? 8000);
-}
+import { NestFactory } from '@nestjs/core';
+import { MessagesModule } from './messages/messages.module';
+import { ValidationPipe } from '@nestjs/common';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessagesModule);
+  const globalPipes = new ValidationPipe();
+  app.useGlobalPipes(globalPipes);
+  await app.listen(process.env.PORT ?? 8000);
+}
 bootstrap();
